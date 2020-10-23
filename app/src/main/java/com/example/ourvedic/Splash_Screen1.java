4d96b5807843577b99822871db45bae08d9febf2
VarunSharma117 <coolvarunsharma117>
2020-10-23 18:08:16 +0530

    Splash Screen added! #VarunSharma

---
+++ app/src/main/java/com/example/ourvedic/Splash_Screen1.java
@@ -0,0 +1,29 @@
+package com.example.ourvedic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Splash_Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash__screen1);
+        int SPLASH_DISPLAY_LENGTH = 1500;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash_Screen1.this,MainActivity.class);
+                Splash_Screen1.this.startActivity(mainIntent);
+                Splash_Screen1.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
